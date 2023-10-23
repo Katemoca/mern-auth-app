d869dac468a3525a30059166c511930c884fb6c7
Katerin <katerinmonguacastro@gmail.com>
2023-10-23 12:55:56 -0400

    connect to the db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { MONGO } = process.env;
+
+mongoose
+  .connect(MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
 const PORT = 3000;
