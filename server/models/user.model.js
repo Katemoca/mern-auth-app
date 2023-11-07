b72735b923d0577092e3ced8dc0bf92c5ca5450a
Katerin <katerinmonguacastro@gmail.com>
2023-11-07 10:05:23 -0500

    created model User

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.Schema("User", userSchema);
+
+export default User;
+
+// Documentation = https://mongoosejs.com/docs/models.html
