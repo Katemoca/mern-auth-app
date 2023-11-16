3e6e19c41aaf451560591a328798f1c501404315
Katerin <katerinmonguacastro@gmail.com>
2023-11-16 14:58:19 -0500

    errorHandler modifications

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,7 +12,7 @@ const signUp = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(errorHandler(300, "Something went wrong"));
+    next(error); // next(errorHandler(300, "Something went wrong"));
   }
 };
 
--- server/utils/error.js
+++ server/utils/error.js
@@ -1,5 +1,7 @@
+// There are sometimes in our application that we need to create a custome error. Probably there's no error but if some type of error happens and it doesn't have a message we can send ours.
+
 const errorHandler = (statusCode, message) => {
-  const error = new Error();
+  const error = new Error(); // Javascript constructor
   error.statusCode = statusCode;
   error.message = message;
   return error;
