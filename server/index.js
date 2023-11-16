f47f147583e1306d54b56c06d3325c08ed8c319c
Katerin <katerinmonguacastro@gmail.com>
2023-11-16 12:41:12 -0500

    created a new database in MongoDB

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json()).listen(PORT, () => {
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
 
-// MIDDLEWARE TO HANDLE ERRORS
+// CUSTOMIZED MIDDLEWARE TO HANDLE ERRORS
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
