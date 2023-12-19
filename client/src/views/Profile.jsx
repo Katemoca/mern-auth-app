4f75c0ca28c7e2bab2745cf22464a30a28b91847
Katerin <katerinmonguacastro@gmail.com>
2023-12-19 15:40:37 -0500

    Adding logic (fb storage) to upload img in Profile.jsx

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -1,15 +1,76 @@
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 function Profile() {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(formData);
+
   const { currentUser } = useSelector((state) => state.user);
+
+  // To upload the image that we have chosen from our files
+  useEffect(() => {
+    if (image) {
+      handleImageUpload(image);
+    }
+  }, [image]);
+
+  const handleImageUpload = async (image) => {
+    const storage = getStorage(app); // We create the storage
+    const fileName = new Date().getTime() + image.name; // We create an unique name for the uploaded image
+    const storageRef = ref(storage, fileName); // We save the image in the firebase storage
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...FormData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        {/* THIS IS THE CONFIGURATION OF THE FIREBASE STORAGE OF OUR PROJECT: This means that the user can only upload files that are images and have a size less than 2MB.
+
+      firebase storage rules:  
+      allow read;
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 &&
+      request.resource.contentType.matches('image/.*') */}
         <img
           src={currentUser.profilePicture}
           alt="profile"
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
         />
         <input
           defaultValue={currentUser.username}
